4dfc8174162dd79f6a41b7dbd263b31099e0dd4f
HackerLime <dilbaryanna@gmail.com>
2024-05-12 15:03:55 +0300

    changes from 12.05.2024

---
+++ client/src/components/UI/icons/SVGProfile.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const SVGProfile = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width={22}
+			height={24}
+			fill="none"
+
+		>
+			<path
+				stroke="#454545"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth={2}
+				d="M21 23v-2.34c0-1.24-.53-2.42-1.47-3.3A5.207 5.207 0 0 0 16 16H6c-1.33 0-2.6.49-3.54 1.36-.94.88-1.46 2.06-1.46 3.3V23"
+			/>
+			<path
+				stroke="#454545"
+				strokeLinejoin="round"
+				strokeWidth={2}
+				d="M10.5 11c-2.77 0-5-2.24-5-5 0-2.77 2.23-5 5-5 2.76 0 5 2.23 5 5 0 2.76-2.24 5-5 5Z"
+			/>
+		</svg>)
+}
+
+export default SVGProfile
