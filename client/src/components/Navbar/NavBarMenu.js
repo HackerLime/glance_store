1d453d0b129256d1ef2637a956e50b3db2aa455e
HackerLime <dilbaryanna@gmail.com>
2024-06-06 06:15:50 +0300

    changes from 05.06,06.06,I’m finalizing the layout so I can start adapting it

--- client/src/components/Navbar/NavBarMenu.js
+++ client/src/components/Navbar/NavBarMenu.js
@@ -1,22 +1,26 @@
 import React, { useContext, useState } from 'react'
-import SVGCatalog from '../UI/icons/SVGCatalog'
-import SVGBasket from '../UI/icons/SVGBasket'
-import SVGProfile from '../UI/icons/SVGProfile'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { ADMIN_ROUTE, BASKET_ROUTE, CATALOG_ROUTE, LOGIN_ROUTE, PROFILE_ROUTE } from '../../router/paths'
 import { Context } from '../../index'
 import { observer } from 'mobx-react-lite'
 import Button from 'react-bootstrap/Button'
+import NavBarMenuItem from './NavBarMenuItem'
+import SVGFBStarF from '../UI/icons/SVGFBStarF'
+import SVGCatalog from '../UI/icons/SVGCatalog'
+import SVGProfile from '../UI/icons/SVGProfile'
+import SVGBasket from '../UI/icons/SVGBasket'
 const NavBarMenu = observer(() => {
+	const [hoverColor, setHoverColor] = useState('#454545')
+
 	const navigate = useNavigate()
-	const menuItems = [
-		{ id: 1, element: <SVGCatalog />, name: 'Каталог', route: { CATALOG_ROUTE } },
-		{ id: 2, element: <SVGBasket />, name: 'Корзина', route: { BASKET_ROUTE } },
-		{ id: 3, element: <SVGProfile />, name: 'Профиль', route: { ADMIN_ROUTE } },
-	]
-	const [catalogColor, setCatalogColor] = useState('#454545')
-	const [basketColor, setBasketColor] = useState('#454545')
-	const [profileColor, setProfileColor] = useState('#454545')
+	const [publicMenuItems, setPublicMenuItems] = useState([
+		{ id: 1, icon: <SVGCatalog color={hoverColor} />, name: 'Каталог', route: CATALOG_ROUTE },
+	])
+	const [privateMenuItems, setPrivateMenuItems] = useState([
+		{ id: 1, icon: <SVGBasket color={hoverColor} />, name: 'Корзина', route: BASKET_ROUTE },
+		{ id: 2, icon: <SVGProfile color={hoverColor} />, name: 'Профиль', route: PROFILE_ROUTE },
+	])
+
 
 	const { user } = useContext(Context)
 
@@ -26,40 +30,22 @@ const NavBarMenu = observer(() => {
 	}
 
 	return (
-		<div className='d-flex'
-			style={{ maxWidth: 230 }}
-		>
-			<Link
-				to={CATALOG_ROUTE}
-				style={{ cursor: 'pointer', textDecoration: 'none', margin: '0 32px 0 0' }}
-				onMouseEnter={() => setCatalogColor('#091d9e')}
-				onMouseLeave={() => setCatalogColor('#454545')}
-				className='d-flex flex-column  align-items-center'>
-				<div ><SVGCatalog color={catalogColor} /></div>
-				<div style={{ color: catalogColor }}>Каталог</div>
-			</Link>
-			{user.isAuth &&
-				<div className='d-flex'>
-					<Link
-						to={BASKET_ROUTE}
-						style={{ cursor: 'pointer', textDecoration: 'none', margin: '0 32px 0 0' }}
-						onMouseEnter={() => setBasketColor('#091d9e')}
-						onMouseLeave={() => setBasketColor('#454545')}
-						className='d-flex flex-column  align-items-center'>
-						<div ><SVGBasket color={basketColor} /></div>
-						<div style={{ color: basketColor }}>Корзина</div>
-					</Link>
-					<Link
-						to={PROFILE_ROUTE}
-						style={{ cursor: 'pointer', textDecoration: 'none', margin: '0 32px 0 0' }}
-						onMouseEnter={() => setProfileColor('#091d9e')}
-						onMouseLeave={() => setProfileColor('#454545')}
-						className='d-flex flex-column  align-items-center'>
-						<div ><SVGProfile color={profileColor} /></div>
-						<div style={{ color: profileColor }}>Профиль</div>
-					</Link>
-				</div>
-			}
+		<div className='d-flex'>
+			<div className='d-flex' style={{ margin: '0 10px 0 0', background: 'gray' }}>
+
+				{publicMenuItems.map(i =>
+					<NavBarMenuItem
+						onMouseEnter={() => console.log('mosueenter')}
+						onMouseLeave={() => console.log('mouseleave')}
+						textcolor={hoverColor} key={i.id} name={i.name} route={i.route} icon={i.icon} />
+				)}
+				{user.isAuth && privateMenuItems.map(i =>
+					<NavBarMenuItem
+						onMouseEnter={() => console.log('mosueenter')}
+						onMouseLeave={() => console.log('mouseleave')}
+						textcolor={hoverColor} key={i.id} name={i.name} route={i.route} icon={i.icon} />
+				)}
+			</div>
 			{user.isAuth ?
 				<div className='d-flex'>
 					<Button className='me-2' onClick={() => logout()} >Выйти</Button>
