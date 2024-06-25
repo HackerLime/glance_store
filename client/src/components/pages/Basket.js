262e8c898cbe4db25de794c58dc910dd943c9e5a
HackerLime <dilbaryanna@gmail.com>
2024-06-25 06:06:15 +0300

    I have set up the cart rendering, in the next version I will try to add styles and functionality

--- client/src/components/pages/Basket.js
+++ client/src/components/pages/Basket.js
@@ -1,38 +1,44 @@
 import React, { useContext, useEffect, useMemo, useState } from 'react'
 import BasketEmpty from '../UI/basket/BasketEmpty'
-import { observer } from 'mobx-react-lite'
 import { Context } from '../../index'
-import { fetchBasketDevices } from '../../http/deviceAPI'
-import { jwtDecode } from 'jwt-decode'
+import DeviceAsList from '../UI/device/deviceaslist/DeviceAsList'
+import Container from 'react-bootstrap/esm/Container'
 
 
 
-const Basket = observer(() => {
+
+const Basket = () => {
 	const { device } = useContext(Context)
-	const [basketDevices, setBasketDevices] = useState([...device.devices])
-	useEffect(() => {
-		fetchBasketDevices().then(data => device.setBasketDevices(data)).catch(e => console.log(e))
-	}, [])
+	const [basketState, setBasketState] = useState([...device.devices])
 
-	const myBasketDevices = () => {
+	useEffect(() => {
 		let arr = []
 		device.basketDevices.map(i => arr.push(i.deviceId))
-		setBasketDevices(basketDevices.filter(i => arr.includes(i.id)))
-
-	}
-	useEffect(() => {
-		myBasketDevices()
+		setBasketState(basketState.filter(i => arr.includes(i.id)))
 	}, [])
-	console.log(basketDevices)
+	const destroyBasketDevice = (id) => {
+		//!Надо прописать после fn AddBasketDevice
+		console.log(`destroyBasketDevice,id=${id}`)
+	}
 
-	if (!basketDevices.length) {
+	if (!basketState.length) {
 		return <div><BasketEmpty /></div>
 	}
 
 	return (
 
-		<div>asas</div>
+		<div className='d-flex justify-content-center' style={{ maxWidth: '1200px', margin: '0px auto' }}>
+			<div style={{ maxWidth: '892px' }} className='d-flex flex-column'>
+				{basketState.map((i, idx) =>
+					<div key={idx}>
+						<p onClick={() => destroyBasketDevice(i.id)} style={{ textAlign: 'end', color: 'red', cursor: 'pointer' }}>Убрать с корзины</p>
+						<DeviceAsList device={i} />
+					</div>
+				)
+				}
+			</div>
+		</div>
 	)
-})
+}
 
 export default Basket
