3a90d58f34aa8a2bc5e036a50c36f29881d0c625
HackerLime <dilbaryanna@gmail.com>
2024-11-12 06:35:36 +0300

    refactoring 12.11

--- client/src/shared/ui/basket/BasketEmpty.tsx
+++ client/src/shared/ui/basket/BasketEmpty.tsx
@@ -1,6 +1,6 @@
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/esm/Button'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { SVGBasketEmpty } from 'shared/assets/icons'
 import { CATALOG_ROUTE } from 'shared/routerPaths'
 
@@ -11,7 +11,7 @@ export const BasketEmpty = () => {
 			<div className='d-flex flex-column align-items-center'>
 				<h1 style={{ margin: '0 0 28px 0', fontWeight: 500, lineHeight: '23px', color: '#0C0C0C' }}>Ваша корзина пуста</h1>
 				<SVGBasketEmpty style={{ margin: '0 0 28px 0' }} />
-				<div style={{ margin: '0 0 28px 0', fontSize: '16px', lineHeight: '19px', color: '#454545' }} to={CATALOG_ROUTE} >Добавьте товары из каталога</div>
+				<Link style={{ margin: '0 0 28px 0', fontSize: '16px', lineHeight: '19px', color: '#454545', textDecoration: 'none' }} to={CATALOG_ROUTE} >Добавьте товары из каталога</Link>
 				<Button style={{ margin: '0 0 40px 0' }} onClick={() => navigate(CATALOG_ROUTE)}>Перейти в каталог</Button>
 			</div>
 		</Container>
--- client/src/shared/ui/device/deviceName/ui/DeviceName.tsx
+++ client/src/shared/ui/device/deviceName/ui/DeviceName.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { DEVICEPAGE_ROUTE } from 'shared/routerPaths';
-import classes from './DeviceName.module.css';
+import styles from './DeviceName.module.css';
 
 type TDeviceNameType = {
 	deviceName: string;
@@ -15,11 +15,11 @@ export const DeviceName: FC<TDeviceNameType> = ({ deviceName, deviceId, brandNam
 	return (
 		<Link
 			to={DEVICEPAGE_ROUTE + '/' + deviceId}
-			className={classes.deviceCarouselText}
+			className={styles.text_wrapper}
 		>
-			<p className={classes.typeName} >{typeName}</p>
-			<p className={classes.brandName} >{brandName}</p>
-			<p className={classes.device__Name}>{deviceName}</p>
+			<p className={styles.typeName} >{typeName}</p>
+			<p className={styles.brandName} >{brandName}</p>
+			<p className={styles.device__Name}>{deviceName}</p>
 		</Link >)
 }
 
